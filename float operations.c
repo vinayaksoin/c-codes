1f6a394e8e421d88659b423c50a78e5f4ff183fa
Vinayak Soin <91793983+vinayaksoin@users.noreply.github.com>
2022-10-08 21:48:47 +0530

    Add files via upload

---
+++ float operations.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main() 
+{
+float a;
+float b;
+printf("enter first integer : ");
+scanf("%f" ,&a);
+printf("enter second integer: ");
+scanf("%f" ,&b);
+float c;
+float d;
+float f;
+c=a+b;
+d=a-b;
+f=a*b;
+float e;
+e=a/b;
+
+printf("     Sum of numbers is :   %f     ",c);
+ printf("      \n   subtraction of numbers is: %f:",d);
+ printf("      \n      quatient of numbers is  %f:",e);
+ printf("    \n    Product of numbers is  %f:",f);
+ 
+ return 0;
+}
